d267c8e40c2a7eabc48ab94fed0c97af2f0c021c
simon van de voort <zeemon777@gmail.com>
2019-11-05 13:57:09 +0100

    first

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+const button = document.getElementById('button');
+const img = document.getElementById('img');
+const address = document.getElementById('address');
+
+button.addEventListener('click', () => {
+  if (address.innerHTML == '') {
+    img.src = 'img/braided-hair.jpg';
+    address.innerHTML = 'rembrandt.peel@gmail.com';
+    // button.style.marginTop = '0';
+
+  } else {
+    img.src = '';
+    address.innerHTML = '';
+    // button.style.marginTop = '494px';
+
+  }
+
+});
+
+
+// img.src = 'img/compute2.jpg';
+// address.innerHTML = 'zeemon777@gmail.com';
+// button.style.opacity = '0';
+// button.style.marginTop = '0';
